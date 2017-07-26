078d8b63ef92c2df063ce77e5688c4a873fd097f
Ryan Frank <ryanfrank@Ryans-MacBook-Pro.local>
2017-07-25 21:35:23 -0600

    completed first api

--- Workshops/s2-Node-Intro/My-First-API/server.js
+++ Workshops/s2-Node-Intro/My-First-API/server.js
@@ -6,9 +6,12 @@ let data = []
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/',express.static('public'))
+
 
 // GET ALL
 app.get('/api',(req,res) => res.json({ message: "Pulling all customer data.", data: data }))
+
 
 // GET ONE
 app.get('/api/:customer',(req,res) => {
@@ -27,11 +30,11 @@ app.get('/api/:customer',(req,res) => {
 app.post('/api',(req,res) => {
   data.push(req.body.name)
   console.log(`Successfully added data: ${data}`)
-  res.json({ message: "Successfully submitted new customer.", data: data})
+  res.json({ message: "Successfully submitted new customer.", data: data })
 })
 
 
-// UPDATE EXISTING CUSTOMER 
+// UPDATE EXISTING CUSTOMER
 app.put('/api/:customer',(req,res) => {
   let searchName = data.indexOf(req.params.customer)
 
@@ -44,4 +47,20 @@ app.put('/api/:customer',(req,res) => {
   }
 })
 
+
+
+//  DELETE EXISTING CUSTOMER
+app.delete('/api/:customer',(req,res) => {
+  let searchName = data.indexOf(req.params.customer)
+
+  if(searchName !== -1){
+    data.splice(searchName, 1)
+    res.json({ message: `Customer successfully deleted: ${req.params.customer}`, data: data })
+  }
+  else{
+    res.json({ message: `Could not find customer ${req.params.customer}`, data: null })
+  }
+})
+
+// SERVE SERVER HOT
 app.listen(port, () => console.log(`Listening on port: ${port}`))
