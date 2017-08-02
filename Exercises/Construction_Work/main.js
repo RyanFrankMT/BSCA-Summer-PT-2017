ac0c3bcbff4246641a87e0d3849143eac15bec34
Ryan Frank <ryanfrank@Ryans-MacBook-Pro.local>
2017-08-01 20:09:30 -0600

    added Vue Menu

--- Exercises/Construction_Work/main.js
+++ Exercises/Construction_Work/main.js
@@ -46,3 +46,35 @@ testCustomer.checking.deposit(1000);
 testCustomer.checking.withdraw(300);
 testCustomer.savings.deposit(2000);
 testCustomer.accountOverview();
+
+
+
+
+
+// EXERCISE 2  ES6 syntax vs. ES5 syntax
+// Both ES6 and ES5 are functionally equivalent
+// ES6 is nice because it is cleaner and makes it more clear that it is a constructor function (purpose is more transparent)
+// Use ES6 from now on
+class Bank{
+  constructor(totalFunds, numAccounts, accounts){
+    this.totalFunds = totalFunds
+    this.numAccounts = accounts
+    this.accounts = accounts
+  }
+  getFunds(){
+    return this.totalFunds
+  }
+  setFunds(funds){
+    this.totalFunds = funds
+  }
+}
+
+function BankES5(totalFunds, numAccounts, accounts){
+  this.totalFunds = totalFunds
+  this.numAccounts = numAccounts
+  this.accounts = accounts
+
+  function getFunds(){
+    return this.totalFunds
+  }
+}
